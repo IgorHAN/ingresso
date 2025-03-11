876d8017c276945dda9301c6b6ccae9c925a6c56
Igor Henrique <igorhenriqueanascimento@gmail.com>
2025-03-11 02:27:49 -0300

    Adicionado as funcitons comprarInferior e comprarSuperior

--- js/app.js
+++ js/app.js
@@ -4,11 +4,13 @@ function comprar() {
 
     if(tipo.value == "pista"){
         comprarPista(qtd);
+    } else if(tipo.value == "inferior"){
+        comprarInferior(qtd);
+    } else if(tipo.value == "superior"){
+        comprarSuperior(qtd);
     }
 
-
-
-}
+    }
 
 function comprarPista(qtd){
     let qtdPista = parseInt(document.getElementById("qtd-pista").textContent);
@@ -17,6 +19,32 @@ function comprarPista(qtd){
     } else{
         qtdPista = qtdPista - qtd;
         document.getElementById("qtd-pista").textContent = qtdPista;
+        alert("Compra realizada!");
+
+    }
+
+}
+
+function comprarInferior(qtd){
+    let qtdInf = parseInt(document.getElementById("qtd-inferior").textContent);
+    if(qtd > qtdInf){
+        alert("Quantidade indisponível para tipo Cadeira Inferior");
+    } else{
+        qtdInf = qtdInf - qtd;
+        document.getElementById("qtd-inferior").textContent = qtdInf;
+        alert("Compra realizada!");
+
+    }
+
+}
+
+function comprarSuperior(qtd){
+    let qtdSup = parseInt(document.getElementById("qtd-superior").textContent);
+    if(qtd > qtdSup){
+        alert("Quantidade indisponível para tipo Cadeira Superior");
+    } else{
+        qtdSup = qtdSup - qtd;
+        document.getElementById("qtd-superior").textContent = qtdSup;
         alert("Compra realizada!");
 
     }
